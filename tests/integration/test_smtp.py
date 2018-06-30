700fd250448373ce9253c942d4b87cb51f2f9f38
Simon Brunning <simon@brunningonline.net>
2018-06-30 15:01:47 +0100

    Aded test for mocking SMTP calls.

---
+++ tests/integration/test_smtp.py
@@ -0,0 +1,37 @@
+import logging
+import smtplib
+
+import pytest
+from hamcrest import assert_that
+
+from matchers.request import email_sent
+from mb.imposters import smtp_imposter
+from tests.utils.builders import message, random_email, random_string
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.usefixtures("mock_server")
+def test_email(mock_server):
+    # Given
+    imposter = smtp_imposter()
+
+    to_email = random_email()
+    from_email = random_email()
+    text = random_string()
+
+    with mock_server(imposter) as s:
+        logger.debug("server: %s", s)
+
+        # When
+        server = smtplib.SMTP()
+        server.connect(host=imposter.host, port=imposter.port)
+        try:
+            server.sendmail(
+                to_email, [from_email], message(to_email=to_email, from_email=from_email, text=text).as_string()
+            )
+        finally:
+            server.quit()
+
+        # Then
+        assert_that(s, email_sent(text=text))
---
+++ tests/utils/builders.py
@@ -0,0 +1,27 @@
+import email
+import random
+import string
+from email.mime.text import MIMEText
+
+
+def message(to_name=None, to_email=None, from_name=None, from_email=None, subject=None, text=None):
+    to_name = to_name or random_string()
+    to_email = to_email or random_email()
+    from_name = from_name or random_string()
+    from_email = from_email or random_email()
+    subject = subject or random_string()
+    text = text or random_string()
+
+    msg = MIMEText(text)
+    msg["To"] = email.utils.formataddr((to_name, to_email))
+    msg["From"] = email.utils.formataddr((from_name, from_email))
+    msg["Subject"] = subject
+    return msg
+
+
+def random_email():
+    return "{}@example.com".format(random_string())
+
+
+def random_string(length=10, characters=string.ascii_letters + string.digits):
+    return "".join(random.choice(characters) for _ in range(length))
