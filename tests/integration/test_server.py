1fd47dd2dc702c8509c073ddb135ce180f01af75
Simon Brunning <simon@brunningonline.net>
2019-02-09 10:00:02 +0000

    Separate integration tests a bit.

---
+++ tests/integration/test_server.py
@@ -0,0 +1,23 @@
+# encoding=utf-8
+import logging
+
+import requests
+from brunns.matchers.response import response_with
+from hamcrest import assert_that, is_
+
+from mbtest.imposters import Imposter, Predicate, Response, Stub
+from mbtest.matchers import had_request
+
+logger = logging.getLogger(__name__)
+
+
+def test_request_to_mock_server(mock_server):
+    # Start mock server with required behavior
+    imposter = Imposter(Stub(Predicate(path="/test"), Response(body="sausages")))
+
+    with mock_server(imposter) as server:
+        # Make request to mock server
+        response = requests.get("{0}/test".format(imposter.url))
+
+        assert_that("We got the expected response", response, is_(response_with(status_code=200, body="sausages")))
+        assert_that("The mock server recorded the request", server, had_request(path="/test", method="GET"))
---
+++ tests/integration/test_stubs.py
@@ -0,0 +1,30 @@
+# encoding=utf-8
+import logging
+
+import requests
+from brunns.matchers.response import response_with
+from hamcrest import assert_that
+
+from mbtest.imposters import Imposter, Predicate, Response, Stub
+
+logger = logging.getLogger(__name__)
+
+
+def test_multiple_stubs(mock_server):
+    imposter = Imposter(
+        [
+            Stub(Predicate(path="/test1"), Response(body="sausages")),
+            Stub(Predicate(path="/test2"), Response(body="chips")),
+        ],
+        port=4567,
+        name="bill",
+    )
+
+    with mock_server(imposter) as s:
+        logger.debug("server: %s", s)
+        r1 = requests.get("{0}/test1".format(imposter.url))
+        r2 = requests.get("{0}/test2".format(imposter.url))
+
+        pass
+    assert_that(r1, response_with(body="sausages"))
+    assert_that(r2, response_with(body="chips"))
