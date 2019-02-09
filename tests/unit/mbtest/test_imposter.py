51086c7a83638ff57d2cc397f06031bbf843e93c
Simon Brunning <simon@brunningonline.net>
2019-02-09 09:49:47 +0000

    Move pure unit tests out of the integration directory.

--- tests/unit/mbtest/test_imposter.py
+++ tests/unit/mbtest/test_imposter.py
@@ -1,11 +1,22 @@
 # encoding=utf-8
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import logging
 
-from mbtest.imposters import Imposter, Stub, Proxy, Response
+from hamcrest import assert_that, has_entries
+
+from mbtest.imposters import Predicate, Imposter, Stub, Response, Proxy
 
 logger = logging.getLogger(__name__)
+
+
+def test_default_predicate():
+    # Given
+    predicate = Predicate()
+
+    # When
+    structure = predicate.as_structure()
+
+    # Then
+    assert_that(structure, has_entries(caseSensitive=True, equals=has_entries()))
 
 
 def test_structure_port():
