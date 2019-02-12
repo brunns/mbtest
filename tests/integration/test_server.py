78806a6d7e71be55959040b7b2a07e4292c3ff95
Simon Brunning <simon@brunningonline.net>
2019-02-12 14:50:11 +0000

    Test for multiple servers.

--- tests/integration/test_server.py
+++ tests/integration/test_server.py
@@ -2,6 +2,7 @@
 import logging
 from pathlib import Path
 
+import platform
 import pytest
 import requests
 from brunns.matchers.response import response_with
@@ -51,3 +52,25 @@ def test_exception_running_multiple_servers_on_same_port():
         finally:
             server1.close()
             server2.close()
+
+
+@pytest.mark.skipif(platform.system() == "Windows", reason="Can only run one server on Windows for some reason.")
+def test_allow_multiple_servers_on_different_ports():
+    # Given
+    try:
+        server1 = MountebankServer(port=2526)
+        server2 = MountebankServer(port=2527)
+        imposter1 = Imposter(Stub(Predicate(path="/test"), Response(body="sausages")))
+        imposter2 = Imposter(Stub(Predicate(path="/test"), Response(body="bacon")))
+
+        with server1(imposter1), server2(imposter2):
+
+            response1 = requests.get("{0}/test".format(imposter1.url))
+            response2 = requests.get("{0}/test".format(imposter2.url))
+
+            assert_that(response1, is_(response_with(status_code=200, body="sausages")))
+            assert_that(response2, is_(response_with(status_code=200, body="bacon")))
+
+    finally:
+        server1.close()
+        server2.close()
