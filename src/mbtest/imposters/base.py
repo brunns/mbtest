66b130a70c293797e1f01eeddf4b73ccb678baa7
Simon Brunning <simon@brunningonline.net>
2019-02-15 10:50:33 +0000

    Separate out imposter, stub, predicate and response classes.

---
+++ src/mbtest/imposters/base.py
@@ -0,0 +1,12 @@
+# encoding=utf-8
+from abc import ABCMeta, abstractmethod
+
+
+class JsonSerializable(object, metaclass=ABCMeta):
+    @abstractmethod
+    def as_structure(self):  # pragma: no cover
+        """
+        :returns Structure suitable for JSON serialisation.
+        :rtype: dict
+        """
+        raise NotImplementedError()
