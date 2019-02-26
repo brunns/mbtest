89e88d2906dc35d5dbf4928a0dc8f7770ec773fc
Colin Schoen <schoen@yelp.com>
2019-02-26 15:03:03 -0800

    Move module level imports to top of file

--- src/mbtest/imposters/imposters.py
+++ src/mbtest/imposters/imposters.py
@@ -1,18 +1,16 @@
 # encoding=utf-8
 from __future__ import unicode_literals, absolute_import, division, print_function
 from six import PY3
+from enum import Enum
+from furl import furl
+
+from mbtest.imposters.base import JsonSerializable
+from mbtest.imposters.stubs import Stub, Proxy
 
 if PY3:
     from collections.abc import Sequence
 else:  # pragma: no cover
     from collections import Sequence
-
-from enum import Enum
-
-from furl import furl
-
-from mbtest.imposters.base import JsonSerializable
-from mbtest.imposters.stubs import Stub, Proxy
 
 
 class Imposter(JsonSerializable):
