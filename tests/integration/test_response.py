ddb6619dd9adbb8a14ed0744b5f632c73d56e8ae
Simon Brunning <simon@brunningonline.net>
2019-02-27 11:26:21 +0000

    Added UsingRegex subclass, with ignore_case option.

--- tests/integration/test_response.py
+++ tests/integration/test_response.py
@@ -7,7 +7,7 @@ import requests
 from brunns.matchers.response import response_with
 from hamcrest import assert_that, is_, has_entry
 
-from mbtest.imposters import Imposter, Response, Stub, Copy, Using
+from mbtest.imposters import Imposter, Response, Stub, Copy, UsingRegex
 
 logger = logging.getLogger(__name__)
 
@@ -61,17 +61,34 @@ def test_multiple_responses(mock_server):
         assert_that(r3, is_(response_with(body="sausages")))
 
 
-def test_copy(mock_server):
+def test_regex_copy(mock_server):
     imposter = Imposter(
         Stub(
             responses=Response(
                 status_code="${code}",
-                copy=Copy("path", "${code}", Using(Using.Method.REGEX, "\\d+")),
+                headers={"X-Test": "${header}"},
+                body="Hello, ${name}!",
+                copy=[
+                    Copy("path", "${code}", UsingRegex("\\d+")),
+                    Copy({"headers": "X-Request"}, "${header}", UsingRegex(".+")),
+                    Copy({"query": "name"}, "${name}", UsingRegex("AL\\w+", ignore_case=True)),
+                ],
             )
         )
     )
 
     with mock_server(imposter):
-        response = requests.get('{imposter_url}/456'.format(imposter_url=imposter.url))
+        response = requests.get(
+            '{imposter_url}/456'.format(imposter_url=imposter.url), params={"name": "Alice"}, headers={"X-REQUEST": "Header value"}
+        )
 
-        assert_that(response, is_(response_with(status_code=456)))
+        assert_that(
+            response,
+            is_(
+                response_with(
+                    status_code=456,
+                    body="Hello, Alice!",
+                    headers=has_entry("X-Test", "Header value"),
+                )
+            ),
+        )
