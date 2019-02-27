3b890d48f8de32a4e5a9f0b531b3f894c2008cc7
Simon Brunning <simon@brunningonline.net>
2019-02-27 11:26:21 +0000

    Added UsingRegex subclass, with ignore_case option.

--- src/mbtest/imposters/__init__.py
+++ src/mbtest/imposters/__init__.py
@@ -2,5 +2,5 @@
 # flake8: noqa
 from .imposters import Imposter, smtp_imposter
 from .predicates import Predicate, TcpPredicate
-from .responses import Response, TcpResponse, Copy, Using
+from .responses import Response, TcpResponse, Copy, UsingRegex
 from .stubs import Stub, Proxy
--- src/mbtest/imposters/responses.py
+++ src/mbtest/imposters/responses.py
@@ -1,4 +1,5 @@
 # encoding=utf-8
+import abc
 from collections.abc import Sequence
 from enum import Enum
 from xml.etree import ElementTree as et  # nosec - We are creating, not parsing XML.
@@ -99,7 +100,18 @@ class Response(JsonSerializable):
 
 
 class Copy(JsonSerializable):
+    """Represents a copy behavior - see http://www.mbtest.org/docs/api/behaviors#behavior-copy"""
+
     def __init__(self, from_, into, using):
+        """
+        :param from_: The name of the request field to copy from, or, if the request field is an object, then an object
+        specifying the path to the request field.
+        :type from_: str or dict(str, str)
+        :param into: The token to replace in the response with the selected request value.
+        :type into: str
+        :param using: The configuration needed to select values from the response
+        :type using: Using
+        """
         self.from_ = from_
         self.into = into
         self.using = using
@@ -108,18 +120,41 @@ class Copy(JsonSerializable):
         return {"from": self.from_, "into": self.into, "using": self.using.as_structure()}
 
 
-class Using(JsonSerializable):
+class Using(JsonSerializable, metaclass=abc.ABCMeta):
     class Method(Enum):
         REGEX = "regex"
         XPATH = "xpath"
         JSONPATH = "jsonpath"
 
     def __init__(self, method, selector):
+        """
+        :param method: The method used to select the value(s) from the request.
+        :type method: Using.Method
+        :param selector: The selector used to select the value(s) from the request.
+        :type selector: str
+        """
         self.method = method
         self.selector = selector
 
     def as_structure(self):
         return {"method": self.method.value, "selector": self.selector}
+
+
+class UsingRegex(Using):
+    def __init__(self, selector, ignore_case=False):
+        """
+        :param selector: The selector used to select the value(s) from the request.
+        :type selector: str
+        :param ignore_case: Uses a case-insensitive regular expression (For REGEX method)
+        :type ignore_case: bool
+        """
+        super().__init__(Using.Method.REGEX, selector)
+        self.ignore_case = ignore_case
+
+    def as_structure(self):
+        structure = super().as_structure()
+        structure["options"] = {"ignoreCase": self.ignore_case}
+        return structure
 
 
 class TcpResponse(JsonSerializable):
