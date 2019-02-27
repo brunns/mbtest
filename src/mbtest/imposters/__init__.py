6247c542678eaa224ef52f3ee6b9008961987f9c
Simon Brunning <simon@brunningonline.net>
2019-02-27 11:26:21 +0000

    Added UsingRegex subclass, with ignore_case option.

--- src/mbtest/imposters/__init__.py
+++ src/mbtest/imposters/__init__.py
@@ -1,7 +1,7 @@
 # encoding=utf-8
 from .imposters import Imposter, smtp_imposter
 from .predicates import Predicate, TcpPredicate
-from .responses import Response, TcpResponse, Copy, Using
+from .responses import Response, TcpResponse, Copy, UsingRegex
 from .stubs import Stub, Proxy
 
 __all__ = Imposter, smtp_imposter, Predicate, TcpPredicate, Response, TcpResponse, Stub, Proxy
