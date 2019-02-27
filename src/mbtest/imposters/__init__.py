d537c06d3676f23c3eaadd12c0896c8d1c1d8f1d
Simon Brunning <simon@brunningonline.net>
2019-02-27 12:18:53 +0000

    Move response behaviors into a package.

--- src/mbtest/imposters/__init__.py
+++ src/mbtest/imposters/__init__.py
@@ -1,7 +1,9 @@
 # encoding=utf-8
+# flake8: noqa
+from .behaviors import Copy, UsingRegex
 from .imposters import Imposter, smtp_imposter
 from .predicates import Predicate, TcpPredicate
-from .responses import Response, TcpResponse, Copy, UsingRegex
+from .responses import Response, TcpResponse
 from .stubs import Stub, Proxy
 
 __all__ = Imposter, smtp_imposter, Predicate, TcpPredicate, Response, TcpResponse, Stub, Proxy
