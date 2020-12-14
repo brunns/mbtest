3c336a647d425e654d73a4fdd41b204bc7d9794c
Georgy <georgy.kulikov@aliexpress.ru>
2020-12-14 12:20:53 +0300

    Precommit fixes

--- tests/integration/test_add_stubs.py
+++ tests/integration/test_add_stubs.py
@@ -1,10 +1,13 @@
 import logging
+import os
+from decimal import Decimal
 
+import pytest
 import requests
 from brunns.matchers.response import is_response
 from hamcrest import assert_that
 
-from mbtest.imposters import Stub, Predicate, Response, Imposter
+from mbtest.imposters import Imposter, Predicate, Response, Stub
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +17,7 @@ def test_not_existing_imposter_single_stub(mock_server):
     port = 4567
     with mock_server([]) as s:
         logger.debug("server: %s", s)
-        mock_server.add_stabs(stub, port)
+        mock_server.add_stubs(stub, port)
         imposter = mock_server.get_imposter_by_port(port)
         r1 = requests.get(f"{imposter.url}/test1")
     assert_that(r1, is_response().with_body("sausages"))
@@ -28,7 +31,7 @@ def test_not_existing_imposter_multiple_stubs(mock_server):
     port = 4567
     with mock_server([]) as s:
         logger.debug("server: %s", s)
-        mock_server.add_stabs(stubs, port)
+        mock_server.add_stubs(stubs, port)
         imposter = mock_server.get_imposter_by_port(port)
         r1 = requests.get(f"{imposter.url}/test1")
         r2 = requests.get(f"{imposter.url}/test2")
@@ -36,6 +39,10 @@ def test_not_existing_imposter_multiple_stubs(mock_server):
     assert_that(r2, is_response().with_body("chips"))
 
 
+@pytest.mark.skipif(
+    float(os.environ.get("MBTEST_VERSION", "2.1")) < 2.1,
+    reason="AddStubs to existing imposter requires Mountebank version 2.1 or higher.",
+)
 def test_existing_imposter_multiple_stubs(mock_server):
     existing_imposter = Imposter(
         [
@@ -51,7 +58,7 @@ def test_existing_imposter_multiple_stubs(mock_server):
     ]
     with mock_server(existing_imposter) as s:
         logger.debug("server: %s", s)
-        mock_server.add_stabs(stubs, existing_imposter.port)
+        mock_server.add_stubs(stubs, existing_imposter.port)
         imposter = mock_server.get_imposter_by_port(existing_imposter.port)
         responses = [requests.get(f"{imposter.url}/test{i}") for i in range(1, 5)]
     for number, response in enumerate(responses, start=1):
