95af842709794bbfb5bbd809d6aba91f2dae9b21
Simon Brunning <simon@brunningonline.net>
2020-12-17 15:28:46 +0000

    Make integration test for add_stubs() a bit more story-telling.

--- tests/integration/test_impostors.py
+++ tests/integration/test_impostors.py
@@ -5,7 +5,7 @@ import os
 import pytest
 import requests
 from brunns.matchers.response import is_response
-from hamcrest import assert_that
+from hamcrest import assert_that, contains_exactly
 
 from mbtest.imposters import Imposter, Predicate, Response, Stub
 from mbtest.matchers import had_request
@@ -50,22 +50,32 @@ def test_imposter_had_request_matcher(mock_server):
     float(os.environ.get("MBTEST_VERSION", "2.1")) < 2.1,
     reason="AddStubs to existing imposter requires Mountebank version 2.1 or higher.",
 )
-def test_existing_imposter_multiple_stubs(mock_server):
-    existing_imposter = Imposter(
-        [
-            Stub(Predicate(path="/test1"), Response(body="response1")),
-            Stub(Predicate(path="/test2"), Response(body="response2")),
-        ],
-        port=4567,
-        name="bill",
-    )
-    stubs = [
-        Stub(Predicate(path="/test3"), Response(body="response3")),
-        Stub(Predicate(path="/test4"), Response(body="response4")),
-    ]
-    with mock_server(existing_imposter) as s:
-        logger.debug("server: %s", s)
-        existing_imposter.add_stubs(stubs)
-        responses = [requests.get(f"{existing_imposter.url}/test{i}") for i in range(1, 5)]
-    for number, response in enumerate(responses, start=1):
-        assert_that(response, is_response().with_body(f"response{number}"))
+def test_add_stubs_to_running_impostor(mock_server):
+    impostor = Imposter(Stub(Predicate(path="/test0"), Response(body="response0")))
+
+    with mock_server(impostor):
+
+        responses = [requests.get(f"{impostor.url}/test{i}") for i in range(3)]
+        assert_that(
+            responses,
+            contains_exactly(
+                is_response().with_body("response0"),
+                is_response().with_body(""),
+                is_response().with_body(""),
+            ),
+        )
+
+        impostor.add_stubs(
+            [
+                Stub(Predicate(path="/test1"), Response(body="response1")),
+            ]
+        )
+        responses = [requests.get(f"{impostor.url}/test{i}") for i in range(3)]
+        assert_that(
+            responses,
+            contains_exactly(
+                is_response().with_body("response0"),
+                is_response().with_body("response1"),
+                is_response().with_body(""),
+            ),
+        )
