f6ebf4d07a47c6c7584e7d3d3c2aedd5a1143673
Justin Backman <jbackman1@bloomberg.net>
2021-06-12 20:26:42 -0400

    Trying to get converage

--- src/mbtest/server.py
+++ src/mbtest/server.py
@@ -27,13 +27,11 @@ logger = logging.getLogger(__name__)
 def find_mountebank_install():
     DEFAULT_MB_NAME = Path("mb.cmd" if platform.system() == "Windows" else "mb")
     if platform.system() != "Windows":
-        # Look for file in user home directory (NPM local install)
-        user_dir = expanduser("~")
-        current_user_bin = Path(user_dir) / "node_modules" / ".bin" / DEFAULT_MB_NAME
-        if current_user_bin.is_file() or current_user_bin.is_symlink():
-            return str(current_user_bin)
         # Try all paths in the users PATH env
-        for PATH in os.environ.get("PATH").split(":"):
+        paths = [str(Path(expanduser("~")) / "node_modules" / ".bin")] + os.environ.get(
+            "PATH"
+        ).split(":")
+        for PATH in paths:
             usr_bin = Path(PATH) / DEFAULT_MB_NAME
             if usr_bin.is_file() or usr_bin.is_symlink():
                 return str(usr_bin)
@@ -195,7 +193,9 @@ class MountebankServer:
         server_info = requests.get(self.server_url)
         imposters = server_info.json()["imposters"]
         for imposter in imposters:
-            yield Imposter.from_structure(requests.get(imposter["_links"]["self"]["href"]).json())
+            yield Imposter.from_structure(
+                requests.get(imposter["_links"]["self"]["href"]).json()
+            )
 
 
 class ExecutingMountebankServer(MountebankServer):
@@ -247,9 +247,13 @@ class ExecutingMountebankServer(MountebankServer):
         super().__init__(port)
         with self.start_lock:
             if self.server_port in self.running:
-                raise MountebankPortInUseException(f"Already running on port {self.server_port}.")
+                raise MountebankPortInUseException(
+                    f"Already running on port {self.server_port}."
+                )
             try:
-                options = self._build_options(port, debug, allow_injection, local_only, data_dir)
+                options = self._build_options(
+                    port, debug, allow_injection, local_only, data_dir
+                )
                 self.mb_process = subprocess.Popen([executable] + options)  # nosec
                 self._await_start(timeout)
                 self.running.add(port)
@@ -297,7 +301,9 @@ class ExecutingMountebankServer(MountebankServer):
                 time.sleep(0.1)
 
         if not started:
-            raise MountebankTimeoutError(f"Mountebank failed to start within {timeout} seconds.")
+            raise MountebankTimeoutError(
+                f"Mountebank failed to start within {timeout} seconds."
+            )
 
         logger.debug("Server started at %s.", self.server_url)
 
