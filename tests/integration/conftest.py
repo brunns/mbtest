841744761d2790c1e4a89f8d99f3a764f8b75dcc
Simon Brunning <simon@brunningonline.net>
2024-08-06 09:24:55 +0100

    Use pytest-docker rather than lovely-pytest-docker since GHA is now only running docker compose V2.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,11 @@
 ﻿# encoding=utf-8
 import os
 import platform
+from urllib.error import HTTPError
 
 import pytest
+import requests
+from requests.exceptions import ConnectionError as RequestsConnectionError
 from yarl import URL
 
 from mbtest import server
@@ -20,8 +23,20 @@ def mock_server(request):
 @pytest.fixture(scope="session")
 def httpbin(docker_ip, docker_services) -> URL:
     if HTTPBIN_CONTAINERISED:
-        docker_services.start("httpbin")
-        port = docker_services.wait_for_service("httpbin", 80)
-        return URL(f"http://{docker_ip}:{port}")
+        port = docker_services.port_for("httpbin", 80)
+        url = URL(f"http://{docker_ip}:{port}")
+        docker_services.wait_until_responsive(
+            timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
+        )
+        return url
     else:
         return URL("https://httpbin.org")
+
+
+def is_responsive(url: URL) -> bool:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return True
+    except (RequestsConnectionError, HTTPError):
+        return False
